f2cc76c7f87bf09cf8940b3cc60d9b1fa4b4cd83
johnprojeects <161773210+johnprojeects@users.noreply.github.com>
2024-04-03 19:19:24 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -27,13 +27,14 @@ app.post('/api/get-posts', async (req, res) => {
         const { category } = req.body;
         let query = req.body.query;
         const regex = /^\s{1,}$/;
+        const msg = ?!red|green|blue;
 
         // Ignore empty space searches
         query.match(regex) ? query = '' : query;
 
         // Match via regex & case insensitive search
         if (category == 'all') {
-            const posts = await Post.find({ $or: [{ title: { $regex: '.*' + query + '.*', $options: 'i' } }, { desc: { $regex: '.*' + query + '.*', $options: 'i' } }] });
+            const posts = await Post.find({ $or: [{ title: { $regex: '.*' + query + '.*', $options: 'i' } }, { desc: { $regex: '.*' + query + '.*', $options: 'i' } }], category: $msg });
             res.json(posts);
         }
         else {
