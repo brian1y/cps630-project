83110255e8d3346878b1f7dc94ae36bd14bab503
brian1y <brian.yang@torontomu.ca>
2024-04-04 12:29:52 -0400

    fixed Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -47,6 +47,12 @@ function Home() {
                                 <li>
                                     <a href='/admin'>Admin Page</a>
                                 </li>
+                                <li>
+                                    <a href='/send-message'>Send Message</a>
+                                </li>
+                                <li>
+                                    <a href='/inbox'>Inbox</a>
+                                </li>
                             </ul>
                         </nav>
                     </section>
@@ -70,35 +76,10 @@ function Home() {
 
     return (
         <main id='landing'>
-            {user ? <p id='welcome-message'>Hello {user}!</p> : ''}
-
-            {/* Debugging stuff; to be removed */}
-            <ul>
-                <li>
-                    <a href='/post-ad'>Post Ad</a>
-                </li>
-                <li>
-                    <a href='/listing'>Post Listing</a>
-                </li>
-                <li>
-                    <a href='/admin'>Admin Page</a>
-                </li>
-                <li>
-                    <a href='/send-message'>Send Message</a>
-                </li>
-                <li>
-                    <a href='/inbox'>Inbox</a>
-                </li>
-            </ul>
             {/* Changes render depending on login state (user included in check as a fail-safe) */}
             {isLoggedIn && user ? loggedInView() : guestView()}
         </main>
     );
 }
-
-// Debugging: to be removed
-Home.defaultProps = {
-    user: 'User',
-};
 
 export default Home;
