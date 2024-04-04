6f2bf8d73a4b7f0d2263e04f10ac8262c54bd051
AndrewYu2 <andrew.yu@torontomu.ca>
2024-04-04 14:44:36 -0400

    Boots guests off user pages, restrict image sizes

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -1,15 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Loading from './Loading';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 function Inbox() {
-    const user = JSON.parse(localStorage.getItem('user')).username
+    const { user } = useAuthContext();
     const [messages, setMessages] = useState('')
     const [first, setFirst] = useState(true);
 
+    const [isLoading, setIsLoading] = useState(true);
+
+    const redirect = new useNavigate();
+
+    // Login check
+    useEffect(() => {
+        if (!user) {
+            if (!isLoading) {
+                redirect('/login', { replace: true });
+            }
+            else {
+                setIsLoading(false);
+            }
+        }
+        // eslint-disable-next-line
+    }, [user, isLoading])
+
+    // Displays loading page, if loading
+    isLoading && <Loading />;
+
     //Used to display messages on first load
     const messagesOnload = () => {
-        const filter = {user: user};
+        const filter = { user: user };
         setFirst(null);
-        
+
         fetch('http://localhost:3001/api/get-messages', {
             method: 'POST',
             headers: {
@@ -17,8 +40,8 @@ function Inbox() {
             },
             body: JSON.stringify(filter)
         })
-        .then(res => res.json())
-        .then(messages => setMessages(messages))
+            .then(res => res.json())
+            .then(messages => setMessages(messages))
     }
 
     //Query DB for messages on first load
@@ -53,12 +76,12 @@ function Inbox() {
         <>
             <section id='message-container'>
                 {messages.map((message) => (
-                    <div style={{border:'double'}}>
+                    <div style={{ border: 'double' }}>
                         <h4>To: {message.title}</h4>
                         <h4>From: {message.url}</h4>
                         <hr></hr>
                         <p>{message.desc}</p>
-                    </div>        
+                    </div>
                 ))}
             </section>
         </>
--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,14 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Loading from './Loading';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 function SendMessage() {
     // States for message information
     const [to, setTo] = useState('')
     const [message, setMessage] = useState('')
-  
+
+    const { user } = useAuthContext();
+    const [isLoading, setIsLoading] = useState(true);
+
+    const redirect = new useNavigate();
+
+    // Login check
+    useEffect(() => {
+        if (!user) {
+            if (!isLoading) {
+                redirect('/login', { replace: true });
+            }
+            else {
+                setIsLoading(false);
+            }
+        }
+        // eslint-disable-next-line
+    }, [user, isLoading])
+
+    // Displays loading page, if loading
+    isLoading && <Loading />;
+
     //Sends message
     const handleSubmit = (e) => {
         const from = JSON.parse(localStorage.getItem('user')).username
-        const newMessage = {to, message, from};
+        const newMessage = { to, message, from };
         setTo('');
         setMessage('');
 
@@ -21,25 +45,25 @@ function SendMessage() {
             body: JSON.stringify(newMessage)
         })
 
-        // Server response
-        .then((resp) => {
-            console.log(resp);
-            if (resp.ok) {
-                alert('Message sent!');
-                resp.json().then(data => console.log(data))
-            }
-            else {
-                resp.json().then(data => alert(data.error))
-            }
-        })
+            // Server response
+            .then((resp) => {
+                console.log(resp);
+                if (resp.ok) {
+                    alert('Message sent!');
+                    resp.json().then(data => console.log(data))
+                }
+                else {
+                    resp.json().then(data => alert(data.error))
+                }
+            })
     };
-  
+
     return (
         <div id='post-page'>
             <h2>
                 Send Message
             </h2>
-            <hr/>
+            <hr />
             <fieldset id='ad-form'>
                 <form onSubmit={handleSubmit}>
                     <label>
@@ -50,11 +74,11 @@ function SendMessage() {
                         <p>Message:</p>
                         <textarea id='message' value={message} onChange={(e) => setMessage(e.target.value)} required />
                     </label>
-                    <input id='submit' type='submit' value='Send Message'/>
+                    <input id='submit' type='submit' value='Send Message' />
                 </form>
             </fieldset>
         </div>
-    )   
+    )
 }
 
 export default SendMessage;
