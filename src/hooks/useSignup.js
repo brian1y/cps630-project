c230098ae47f85049475c753216fd7d7a79cf7d0
brian1y <118472529+brian1y@users.noreply.github.com>
2024-04-05 21:16:35 -0400

    test frontend

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -10,7 +10,7 @@ export const useSignup = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('https://cps630-project.onrender.com/api/user/signup', {
+        const response = await fetch('https://cps630-project-api.onrender.com/api/user/signup', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
